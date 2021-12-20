aec413e1d70369384257e330e206a0e3481bad7d
akku <akku2883@gmail.com>
2021-12-20 18:52:36 +0000

    updated homepage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //apiBaseUrl: 'https://foodinventoryuk.herokuapp.com/v1/'
-  apiBaseUrl: 'https://foodinventorygerman.herokuapp.com/v1/'
-};  
-``
+  apiBaseUrl: 'https://foodinventorygerman.herokuapp.com/v1/',
+  cover: 'https://foodinventorygerman.herokuapp.com/v1/restaurantService/downloadRestaurantImage?option=COVER&id='
+};
